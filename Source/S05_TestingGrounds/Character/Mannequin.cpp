792dda2e44ede7330e5ca2c265f45bc8fbacdda1
DESKTOP-T3794MB\Kulagin <sergeant.coolagin@gmail.com>
2021-04-19 17:11:45 +0300

    End of video 282.
    Reparent BP_Character to Mannequin class.
    Convert component hierarchy to C++ Mannequin class.

--- Source/S05_TestingGrounds/Character/Mannequin.cpp
+++ Source/S05_TestingGrounds/Character/Mannequin.cpp
@@ -2,19 +2,50 @@
 
 #include "S05_TestingGrounds/Character/Mannequin.h"
 
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "S05_TestingGrounds/Weapons/Gun.h"
+
 // Sets default values
 AMannequin::AMannequin()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Create a CameraComponent	
+	FirstPersonCameraComponentNew = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	FirstPersonCameraComponentNew->SetupAttachment(GetCapsuleComponent());
+	FirstPersonCameraComponentNew->SetRelativeLocation(FVector(-39.56f, 1.75f, 64.f)); // Position the camera
+	FirstPersonCameraComponentNew->bUsePawnControlRotation = true;
+
+	// Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
+	Mesh1PNew = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh1P"));
+	Mesh1PNew->SetOnlyOwnerSee(true);
+	Mesh1PNew->SetupAttachment(FirstPersonCameraComponentNew);
+	Mesh1PNew->bCastDynamicShadow = false;
+	Mesh1PNew->CastShadow = false;
+	Mesh1PNew->SetRelativeRotation(FRotator(1.9f, -19.19f, 5.2f));
+	Mesh1PNew->SetRelativeLocation(FVector(-0.5f, -4.4f, -155.7f));
 }
 
 // Called when the game starts or when spawned
 void AMannequin::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if(GunBlueprint == nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Gun blueprint missing."));
+		return;
+	}
+	Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
+	Gun->AttachToComponent(Mesh1PNew, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+	Gun->AnimInstance = Mesh1PNew->GetAnimInstance();
 	
+}
+
+void AMannequin::Fire() {
+	Gun->OnFire();
 }
 
 // Called every frame
--- Source/S05_TestingGrounds/Character/Mannequin.h
+++ Source/S05_TestingGrounds/Character/Mannequin.h
@@ -6,6 +6,7 @@
 #include "GameFramework/Character.h"
 #include "Mannequin.generated.h"
 
+class UCameraComponent;
 UCLASS()
 class S05_TESTINGGROUNDS_API AMannequin : public ACharacter
 {
@@ -15,15 +16,32 @@ public:
 	// Sets default values for this character's properties
 	AMannequin();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	TSubclassOf<class AGun> GunBlueprint;
+
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	void Fire();
+private:
+
+	/** Pawn mesh: 1st person view (arms; seen only by self) */
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	USkeletalMeshComponent* Mesh1PNew;
+
+	/** First person camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FirstPersonCameraComponentNew;
+
+	UPROPERTY(VisibleAnywhere, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	AGun* Gun;
 };
